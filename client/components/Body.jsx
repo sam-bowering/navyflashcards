d85dcea26796e51c5b2848660fec5ed632903b59
Sam Bowering <samjbowering@gmail.com>
2021-01-25 15:25:48 +1300

    Added anchor background

---
+++ client/components/Body.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Body extends React.Component {
+  state = {
+    style: {
+      backgroundColor: 'black'
+    }
+  }
+
+  render () {
+    return (
+      <div className='body'>
+
+      </div>
+    )
+  }
+}
+
+export default Body
